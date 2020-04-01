7d2f2ea3cf2fd74ce4b17350de85d3192f0f87e9
minhaz <minhaz.taher@gmail.com>
2020-04-01 11:44:40 +0600

    app.get created'

--- mongo/index.js
+++ mongo/index.js
@@ -3,7 +3,18 @@
  * 2. install node express => npm install express --save
  * 3. import express n file => var express = require('express')
                                 var app = express()
+                                
+    4. set server listen PORT number => app.listen(3000)
+    5. create getResponse  = app.get("/", (req, res) => {
+                                res.send("send response");
+                            });
  */
 
 const express = require("express");
 const app = express(); //creating express Application
+
+app.get("/", (req, res) => {
+  res.send("send response");
+});
+
+app.listen(3000); //listing port number
