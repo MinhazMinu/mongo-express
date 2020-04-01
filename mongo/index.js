bfd214b73ec782477c86b4e1b735b8ea2fdebd64
minhaz <minhaz.taher@gmail.com>
2020-04-01 11:39:32 +0600

    listen por number 3000 setup

---
+++ mongo/index.js
@@ -0,0 +1,9 @@
+/**
+ *  1. install npm in the directory => npm init
+ * 2. install node express => npm install express --save
+ * 3. import express n file => var express = require('express')
+                                var app = express()
+ */
+
+const express = require("express");
+const app = express(); //creating express Application
